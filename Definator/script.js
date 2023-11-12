b238e414d5f70fdbf5a6e7212e126af7f8df5ea1
Sarfaraz Unar <130893009+sarfarazunarr@users.noreply.github.com>
2023-11-12 15:54:47 +0500

    Update script.js

--- Definator/script.js
+++ Definator/script.js
@@ -1,19 +1,18 @@
-function writeDefinition(e) {
-    let definition = document.getElementById('definition');
+function writeDefinition(){
+    let definitionholder = document.getElementById('definition');
     let showword = document.getElementById('showword')
     let word = document.getElementById('word').value;
     showword.innerText = word;
     $.ajax({
         method: 'GET',
         url: 'https://api.api-ninjas.com/v1/dictionary?word=' + word,
-        headers: { 'X-Api-Key': 'Enter your API Key' },
+        headers: { 'X-Api-Key': 'YOUR_API_KEY'},
         contentType: 'application/json',
-        success: function (result) {
-            definition.innerText = (result.definition)
+        success: function(result) {
+            definitionholder.innerText = (result.definition);
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
     });
-
 }
